08d9ef3713b831e7f0d76cb5a953f392daa6dba5
Clintoniwoloma001 <tamunosikiiwolomaclinton@gmail.com>
2026-08-08 15:26:12 +0100

    Change BrowserRouter to HashRouter in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { useAuth } from './hooks/useAuth'
 import Layout from './components/Layout'
 import Login from './pages/Login'
@@ -35,7 +35,7 @@ function RoleRoute({ children, roles }) {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Protected><Dashboard /></Protected>} />
@@ -47,6 +47,6 @@ export default function App() {
         <Route path="/users" element={<Protected><RoleRoute roles={['admin']}><Users /></RoleRoute></Protected>} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
